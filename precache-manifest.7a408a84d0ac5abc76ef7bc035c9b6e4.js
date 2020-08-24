596994aed757e961de4a75685b2d767d125a93a2
andikaap13 <50222017+andikaap13@users.noreply.github.com>
2020-08-25 02:16:36 +0700

    Updates

---
+++ precache-manifest.7a408a84d0ac5abc76ef7bc035c9b6e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f553a45a8f65359471b4ae286d1807c",
+    "url": "/reactresume/index.html"
+  },
+  {
+    "revision": "1dd5f4b82969ebb77961",
+    "url": "/reactresume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0af4ec9df38d0eff1d5",
+    "url": "/reactresume/static/js/2.4d8d0fdc.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/reactresume/static/js/2.4d8d0fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd5f4b82969ebb77961",
+    "url": "/reactresume/static/js/main.9c435ce9.chunk.js"
+  },
+  {
+    "revision": "d27141058550e5e610ea",
+    "url": "/reactresume/static/js/runtime-main.503e296e.js"
+  },
+  {
+    "revision": "256f715c629854f422d86658e2f9f7e6",
+    "url": "/reactresume/static/media/cover-bg.256f715c.jpg"
+  },
+  {
+    "revision": "4a31df2c3bb66fd41b25450059c492f9",
+    "url": "/reactresume/static/media/data.4a31df2c.png"
+  },
+  {
+    "revision": "62e8fb30991b8ebd2e7fdf0651547cf1",
+    "url": "/reactresume/static/media/palette.62e8fb30.png"
+  },
+  {
+    "revision": "c332255195f39fa44b07fa132e127257",
+    "url": "/reactresume/static/media/profile-pic.c3322551.jpg"
+  }
+]);
